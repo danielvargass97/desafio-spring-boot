9f77c869bb35072e4567bcfd2d15068df7891d85
Daniel Vargas <daniel.vrgs97@gmail.com>
2026-04-07 11:55:27 -0500

    build: add openapi generator plugin

---
+++ src/test/java/com/nuevospa/tasks/TasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuevospa.tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
